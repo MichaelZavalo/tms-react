a41ee944fecd5aa9208d0ad7597ccb0afb9af639
MichaelZavalo <mihailzavalo@yandex.ru>
2020-09-16 18:18:27 +0300

    feat: add methods 'root','insert'

--- index.js
+++ index.js
@@ -1,46 +1,97 @@
-class BinarySearchTree {
-    constructor(value) {
+//create class Noda for work with class BinarySearchThree
+class Noda {
+    constructor(key, value) {
+        this.key = key;
         this.value = value;
-    }
 
+        this.left = null;
+        this.right = null;
+    }
+}
+//for example
+let node = new Noda(1,"one");
+// create class BinarySearchThree
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    //done
     root() {
-
-        return 
+        return this.root;
     }
-
+    // done
     insert(key, value) {
+        let newNode = new Noda(key, value);
 
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
         return this;
     }
 
+    insertNode(node, newNode) {
+        if (newNode.key < node.key) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    inOrderTraverse() {
+        if (node != null) {
+            this.inOrderTraverse(node.left, callback);
+            callback(node.data);
+            this.inOrderTraverse(node.right, callback);
+        }
+    }
+    //doesn't work
     delete(key) {
 
         return this;
     }
-
+    //doesn't work
     search(key) {
-
-        return this;
+        if (key === null) {
+            return null;
+        } else if (key < node.key) {
+            return this.search(node.left, key);
+        } else if (key > this.key) {
+            return this.search(node.right, key);
+        } else {
+            return this.value;
+        }
     }
-
+    //доделать 
     contains(value) {
-
-        return this;
+        if(node.value === value){
+            return true;    
+        } else this.inOrderTraverse
     }
-    
+    //doesn't work
     traverse(order) {
 
     }
-
+    //doesn't work
     verify() {
 
     }
 }
 
-
-
-//examples
 const bst = new BinarySearchTree();
+bst.insert(2, 'two').insert(1, 'one').insert(3, 'three');
+/* 
+//examples
+
 
 bst.insert(2, 'two').insert(1, 'one').insert(3, 'three');
 
@@ -52,7 +103,7 @@ bst.root(); // 'two'
 
 bst.delete(1).delete(3);
 
- //---2----
+//---2----
 //---------
 //----------
 
@@ -69,8 +120,7 @@ bst.search(1); // 'one'
 bst.contains('three'); // true
 
 
-//examples
-const bst = new BinarySearchTree();
+//examples Advanse methods*
 
 bst.insert(2, 'two').insert(1, 'one').insert(3, 'three');
 
@@ -82,4 +132,4 @@ bst.traverse(true); // ['one', 'two', 'three']
 bst.traverse(false); // ['three', 'two', 'one']
 
 bst._root.key = 0;
-bst.verify(); //false
+bst.verify(); //false */
